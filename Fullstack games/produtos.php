dfd2ad2f72ebd97856817dedf61bc4eb8487084f
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2020-11-09 21:19:36 -0300

    Inserção da linha para responsividade no mobile

--- Fullstack games/produtos.php
+++ Fullstack games/produtos.php
@@ -15,6 +15,7 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Produtos - Full stack games</title>
     <link rel="stylesheet" href="./Css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
